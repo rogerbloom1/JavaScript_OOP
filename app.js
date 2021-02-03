174da7ea6aa72143f7ebaf5199b9c9b41783f8ae
Roger Bloom <rogerbloom1@gmail.com>
2021-02-03 06:55:21 -0800

    =both exercises working

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+class Person {
+    constructor(name, pets, residence, hobbies) {
+    this.name = name;
+    this.pets = pets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+    }
+    info() {
+        console.log("My name is " + this.name + ". I have " + this.pets + " pets, my residence is " + this.residence + " and my hobbies are " + this.hobbies + ".")
+    }
+    soundOff() {
+        console.log("Hello, fellow person!")
+    }
+}
+let firstPerson = new Person("Tim", 2, "McKinleyville", ["painting", "guitar", "poor spelling"]);
+firstPerson.soundOff();
+firstPerson.info();
+
+class Coder extends Person {
+    constructor(name, pets, residence, hobbies) {
+    super(name, pets, residence, hobbies);
+    this.occupation = "Full Stack Web Developer"
+    }
+    soundOff() {
+        console.log("Hello, fellow person! I'm a Coder!")
+    }
+}
+let firstCoder = new Coder("Tom", 3, "Centerville", [chess, MMA]);
+firstCoder.soundOff()
